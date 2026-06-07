f84a2cd82c194b4127d5beb41b0710633fc21d5e
mdsahib5351-ctrl <mdsahib5351@gmail.com>
2026-06-07 05:47:10 +0530

    Delete app directory

--- app/src/main/java/com/zopchat/ChatRow.java
+++
@@ -1,3 +0,0 @@
-package com.zopchat;
-import com.google.firebase.Timestamp;
-public class ChatRow { public String chatId, otherUid, name, mobile, lastMessage; public Timestamp time; }
--- app/src/main/java/com/zopchat/MessageRow.java
+++
@@ -1,3 +0,0 @@
-package com.zopchat;
-import com.google.firebase.Timestamp;
-public class MessageRow { public String id, senderId, text, type; public Timestamp createdAt; }
